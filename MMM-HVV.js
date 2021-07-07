aebdf026a535075f60565d8270b96a12be4d915b
Luca Oelsner <post@lucaoelsner.de>
2021-07-07 17:22:52 +0200

    Adds prettier, formatted code

--- MMM-HVV.js
+++ MMM-HVV.js
@@ -3,76 +3,90 @@ Module.register('MMM-HVV', {
     animationSpeed: 1000,
     maxDepartureTime: 20,
     showIcons: true,
-    header: 'HVV Departures'
+    header: 'HVV Departures',
   },
 
   start: function () {
-    Log.log('HVV frontend started. ' + `Departures from station ${this.config.station} to destination ${this.config.destination}.`)
-    this.sendSocketNotification('HVV_START')
+    Log.log(
+      'HVV frontend started. ' +
+        `Departures from station ${this.config.station} to destination ${this.config.destination}.`
+    );
+    this.sendSocketNotification('HVV_START');
   },
 
   getStyles: function () {
-    return ['MMM-HVV.css']
+    return ['MMM-HVV.css'];
   },
 
   getScripts: function () {
-    return ['moment.js']
+    return ['moment.js'];
   },
 
   fetchHVV: function () {
-    var destination = this.config.destination ? `&direction=${this.config.destination}` : ``
-    var endpoint = `https://v5.hvv.transport.rest/stops/${this.config.station}/departures?duration=${this.config.maxDepartureTime}${destination}`
+    var destination = this.config.destination
+      ? `&direction=${this.config.destination}`
+      : ``;
+    var endpoint = `https://v5.hvv.transport.rest/stops/${this.config.station}/departures?duration=${this.config.maxDepartureTime}${destination}`;
 
     fetch(endpoint)
-      .then(response => {
-        return response.json()
+      .then((response) => {
+        return response.json();
       })
-      .then(data => {
+      .then((data) => {
         for (var key in data) {
-          var line = data[key].line.name
-          var direction = data[key].direction
-          var when = moment(data[key].when).fromNow()
-          var icon = this.config.showIcons ? `<td class="icon"><img class="grayscale" src="https://cloud.geofox.de/icon/linename?name=${line}&height=20&outlined=true&fileFormat=SVG"/></td>` : ''
-          var row = document.createElement('tr')
-          row.innerHTML = `<td class="direction">${direction}</td>` + icon + `<td class="time bright">${when}</td>`
-          table = document.getElementById('results')
-          table.appendChild(row)
+          var line = data[key].line.name;
+          var direction = data[key].direction;
+          var when = moment(data[key].when).fromNow();
+          var icon = this.config.showIcons
+            ? `<td class="icon"><img class="grayscale" src="https://cloud.geofox.de/icon/linename?name=${line}&height=20&outlined=true&fileFormat=SVG"/></td>`
+            : '';
+          var row = document.createElement('tr');
+          row.innerHTML =
+            `<td class="direction">${direction}</td>` +
+            icon +
+            `<td class="time bright">${when}</td>`;
+          table = document.getElementById('results');
+          table.appendChild(row);
         }
       })
-      .catch(error => {
-        Log.error(this.name + ': Something went wrong. Please check modules config. ' + error)
-      })
+      .catch((error) => {
+        Log.error(
+          this.name +
+            ': Something went wrong. Please check modules config. ' +
+            error
+        );
+      });
   },
 
   getDom: function () {
-    var div = document.createElement('div')
-    div.id = "HVV"
+    var div = document.createElement('div');
+    div.id = 'HVV';
 
-    var header = document.createElement('header')
-    header.innerHTML = this.config.header
-    div.appendChild(header)
+    var header = document.createElement('header');
+    header.innerHTML = this.config.header;
+    div.appendChild(header);
 
-    var table = document.createElement('table')
-    table.id = 'results'
-    table.className = 'small'
-    div.appendChild(table)
+    var table = document.createElement('table');
+    table.id = 'results';
+    table.className = 'small';
+    div.appendChild(table);
 
     if (this.config.station) {
-      this.fetchHVV()
+      this.fetchHVV();
     } else {
-      table.innerHTML = `<p class="bright">Station/stop is not defined in config.</p>`
-      Log.error(this.name + ': Station not defined in config.')
+      table.innerHTML = `<p class="bright">Station/stop is not defined in config.</p>`;
+      Log.error(this.name + ': Station not defined in config.');
     }
 
-    return div
+    return div;
   },
 
   notificationReceived: function (notification) {
-    var self = this
+    var self = this;
 
     if (notification === 'CLOCK_MINUTE') {
-      self.updateDom()
-      Log.info('New HVV departures available.')
+      self.updateDom();
+      Log.info('New HVV departures available.');
     }
   },
-})
+});
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # MMM-HVV 🚇
+
 MagicMirror Module that displays departure information about public transportation in Hamburg, Germany. It uses [`v5.hvv.transport.rest`](https://v5.hvv.transport.rest/) (also see [hvv-rest](https://github.com/derhuerst/hvv-rest)).
 
 ![Preview](.github/preview.png)
 
 ## Installation
+
 Clone this module into your MagicMirror's `modules` directory and install dependencies:
 
 ```sh
@@ -14,36 +16,39 @@ git clone https://github.com/lucoel/MMM-HVV
 Now make changes to your `config.js` file.
 
 ## How to use this module
+
 Add this to your `config/config.js` file.
 
 ```javascript
 {
-    /* ...your other config */
+  /* ...your other config */
 
-    modules: [
+  modules: [
+    /* ...your other modules */
 
-        /* ...your other modules */
-
-        {
-            module: 'MMM-HVV',
-            position: 'bottom_left',
-            config: {
-                station: Number,
-            }
-        }
-    ]
+    {
+      module: 'MMM-HVV',
+      position: 'bottom_left',
+      config: {
+        station: Number,
+      },
+    },
+  ];
 }
 ```
 
 ### Find your station/stop ID
+
 > This step is definitely not ideal. Maybe there will be a better solution in the future.
 
 The REST API can [`GET /locations`](https://v5.hvv.transport.rest/api.html#get-locations).
 
 ### Example
+
 ```sh
 curl 'https://v5.hvv.transport.rest/locations?query=fischmarkt&results=1' -s | jq
 ```
+
 ```jsonc
 [
   {
@@ -65,14 +70,13 @@ curl 'https://v5.hvv.transport.rest/locations?query=fischmarkt&results=1' -s | j
 
 With this method you can of course also search for your (optional) direction ID.
 
-
 ## Configuration Options
 
-| Option | Description | Default |
-| ------------- | ------------- | ------------- |
-| `station`  |  The station/stop ID where you want to start | **REQUIRED** |
-| `destination`  | Only show departures for a specific direction/destination ID | null |
-| `maxDepartureTime`  | Maximum time until departure in minutes | 20 |
-| `showIcons`  | Show line icons for every departure | true |
-| `header`  | Custom header text | HVV Departures |
-| `animationSpeed`  | Animation speed to display/hide new results. (WIP) | 1000 |
+| Option             | Description                                                  | Default        |
+| ------------------ | ------------------------------------------------------------ | -------------- |
+| `station`          | The station/stop ID where you want to start                  | **REQUIRED**   |
+| `destination`      | Only show departures for a specific direction/destination ID | null           |
+| `maxDepartureTime` | Maximum time until departure in minutes                      | 20             |
+| `showIcons`        | Show line icons for every departure                          | true           |
+| `header`           | Custom header text                                           | HVV Departures |
+| `animationSpeed`   | Animation speed to display/hide new results. (WIP)           | 1000           |
